76f28b0a425b6cb6652ba9fe86f9a1b880edd949
md-mp4 <mohammoddua@gmail.com>
2026-02-03 01:33:41 +0000

    Unity Setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5; // variable for the speed of the player
+    public Rigidbody2D rb; // reference to the player's RigidBody 2D
+    
+
+    void Start() // Runs only when the class is initialised
+    {
+        
+    }
+
+    void FixedUpdate() // Runs 50x a second continuously
+    {
+        var keyboard = Keyboard.current; // variable to check the curent state of the keyboard
+        if (keyboard == null) return; // check for if no keyboard input is recieved
+        Vector2 movement = new Vector2(0,0); // initialises "movement" vector + sets to 0 at beginning
+
+        // checks if a certain key is pressed
+        if (keyboard.wKey.isPressed)
+        {
+            movement.y = 1;
+        }
+        if (keyboard.sKey.isPressed)
+        {
+            movement.y = -1;
+        }
+        if (keyboard.dKey.isPressed)
+        {
+            movement.x = 1;
+        }
+        if (keyboard.aKey.isPressed)
+        {
+            movement.x = -1;
+        }
+        
+        // sets the player's velocity to the movement vector * speed
+        rb.linearVelocity = movement * speed;
+    }
+}
