e6a5b7fd8f246e662abe0309ca0647cd2ee4753d
md-mp4 <mohammoddua@gmail.com>
2026-02-04 02:13:41 +0000

    day 1 part 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5; // variable for the speed of the player
     public float acceleration = 10f; // variable for player acceleration
     public Rigidbody2D rb; // reference to the player's RigidBody 2D
-    
+    public Animator animator1; // reference to the player's Animator
+    public int direction = 1; // variable to define the direction the player is facing
 
     void Start() // Runs only when the class is initialised
     {
@@ -37,8 +38,23 @@ public class PlayerMovement : MonoBehaviour
             movement.x = -1;
         }
 
-        // sets the player's velocity to the movement vector * speed
+        if(movement.x > 0 && transform.localScale.x < 0 || movement.x < 0 && transform.localScale.x > 0) {
+            Flip();
+        }
+
+        // Send the direction of movement to the animator
+        animator1.SetFloat("horizontal", Mathf.Abs(movement.x));
+        animator1.SetFloat("vertical", Mathf.Abs(movement.y));
+
+        // defines the player's movement and gives them velocity and acceleration
         movement = movement.normalized;
         rb.linearVelocity = Vector2.MoveTowards(rb.linearVelocity, movement * speed, acceleration * Time.fixedDeltaTime);
+    }
+
+    // function used to flip the player's sprite
+    void Flip()
+    {
+        direction *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
