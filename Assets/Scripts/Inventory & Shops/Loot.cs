46fc39292ddce010a63d4628d01c197f31297a5a
md-mp4 <mohammoddua@gmail.com>
2026-02-25 13:28:31 +0000

    day 25 p4

--- Assets/Scripts/Inventory & Shops/Loot.cs
+++ Assets/Scripts/Inventory & Shops/Loot.cs
@@ -8,5 +8,12 @@ public class Loot : MonoBehaviour
 
     public int quantity; // defines the amount of the loot item given
 
+    private void OnValidate() // runs whenever the Unity inspector is used to edit the loot
+    {
+        if (itemSO == null) // if there is no itemSO dictated
+            return; // exit method
 
+        sr.sprite = itemSO.icon; // sets the loot object sprite to the itemSo sprite
+        this.name = itemSO.itemName; // sets the loot object name to the name of the itemSO
+    }
 }
