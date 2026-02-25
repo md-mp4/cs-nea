5106af00ae17a24e55c261ddb7292845e089d38a
md-mp4 <mohammoddua@gmail.com>
2026-02-25 12:25:11 +0000

    day 25 p3

---
+++ Assets/Scripts/Inventory & Shops/Loot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    public ItemSO itemSO; // ItemSO script reference
+    public Animator anim; // animator reference
+    public SpriteRenderer sr; // sprite renderer reference
+
+    public int quantity; // defines the amount of the loot item given
+
+
+}
